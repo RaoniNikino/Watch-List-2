658e38019c30daac2804957abb948c8df2b99fa6
RaoniPirat <raoni.pirat@gmail.com>
2022-02-03 12:58:19 +0100

    associations done

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,7 +2,8 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
 
+  validates :comment, presence: true, length: { minimum: 6 }
   validates_associated :movie
   validates_associated :list
-
+  validates :movie, uniqueness: { scope: :list }
 end
